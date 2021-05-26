1522220be026818c6eab5096d04040e4dbdb63c2
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-05-26 16:07:18 +0100

    refactor: alphabetise for improved readability

--- server/plugins/router.js
+++ server/plugins/router.js
@@ -1,22 +1,22 @@
 const routes = [].concat(
-  require('../routes/auth'),
-  require('../routes/home'),
   require('../routes/account'),
+  require('../routes/auth'),
   require('../routes/contact-add'),
   require('../routes/contact-edit'),
   require('../routes/contact-remove'),
+  require('../routes/home'),
   require('../routes/info'),
   require('../routes/message-create'),
+  require('../routes/message-edit'),
+  require('../routes/message-view'),
   require('../routes/messages'),
-  // require('../routes/message'),
-  // require('../routes/edit-message'),
   // require('../routes/confirm-delete'),
   // require('../routes/confirm-deactivate'),
   // require('../routes/confirm-approve'),
   // require('../routes/confirm-send'),
   // require('../routes/upload'),
-  require('../routes/status'),
-  require('../routes/public')
+  require('../routes/public'),
+  require('../routes/status')
 )
 
 module.exports = {
