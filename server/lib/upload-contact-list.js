baded0c339eae674a873b74227a2613c100bd3bc
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-06-04 12:09:23 +0100

    feat: upload contact list to initiate message sending

---
+++ server/lib/upload-contact-list.js
@@ -0,0 +1,13 @@
+const { BlockBlobClient } = require('@azure/storage-blob')
+const { contactListContainer, contactListStorageConnectionString } = require('../config')
+
+module.exports = async (message) => {
+  const { contacts, id, text } = message
+  const data = JSON.stringify({
+    contacts,
+    message: text
+  })
+  const client = new BlockBlobClient(contactListStorageConnectionString, contactListContainer, `${id}.json`)
+  const res = await client.upload(data, data.length, { blobHTTPHeaders: { blobContentType: 'application/json' } })
+  return res.errorCode === undefined
+}
--- server/routes/message-send.js
+++ server/routes/message-send.js
@@ -1,5 +1,5 @@
-const Joi = require('joi')
 const boom = require('@hapi/boom')
+const Joi = require('joi')
 
 const addAuditEvent = require('../lib/add-audit-event')
 const BaseModel = require('../lib/model')
@@ -7,10 +7,12 @@ const { getMessage, getUsers, updateMessage } = require('../lib/db')
 const getMessageRows = require('../lib/get-message-rows')
 const { messageStates, textMessages: { oneMessageCost } } = require('../constants')
 const { scopes } = require('../permissions')
+const uploadContactList = require('../lib/upload-contact-list')
 
 class Model extends BaseModel {}
 
-function getPhoneNumbersToSendTo (users, message) {
+async function getPhoneNumbersToSendTo (message) {
+  const users = await getUsers()
   const phoneNumbers = []
   users.forEach(user => {
     user.phoneNumbers?.forEach(pn => {
@@ -57,9 +59,9 @@ module.exports = [
     path,
     handler: async (request, h) => {
       const message = await verifyRequest(request)
-      const users = await getUsers()
 
-      const phoneNumbersToSendTo = getPhoneNumbersToSendTo(users, message)
+      // TODO: if there are no contacts to send the message to - do not allow it to be sent
+      const phoneNumbersToSendTo = await getPhoneNumbersToSendTo(message)
       const contactCount = phoneNumbersToSendTo.length
       const cost = contactCount * oneMessageCost
 
@@ -86,10 +88,24 @@ module.exports = [
     handler: async (request, h) => {
       const message = await verifyRequest(request)
 
+      // TODO: if there are no contacts to send the message to - do not allow it to be sent
+      const phoneNumbersToSendTo = await getPhoneNumbersToSendTo(message)
+      const contactCount = phoneNumbersToSendTo.length
+      const cost = contactCount * oneMessageCost
+
+      message.cost = cost
+      message.contacts = phoneNumbersToSendTo
+      message.contactCount = contactCount
+      message.state = messageStates.sent
+
       // TODO: Upload the message criteria to blob storage. If successful, continue else error
       // client.uploadData(message) - needs implementation
+      // await uploadContactList(message)
+      const uploadRes = await uploadContactList(message)
+      if (!uploadRes) {
+        return boom.internal(`Problem uploading contact list for message ${message.id}.`)
+      }
 
-      message.state = messageStates.sent
       const { user } = request.auth.credentials
       addAuditEvent(message, user)
       const res = await updateMessage(message)
