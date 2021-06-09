9fe25b435dcd1b5066efcccff66b80e1b85cc75a
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-06-09 10:47:06 +0100

    feat: initial add contact tests

---
+++ test/server/routes/contact-add.test.js
@@ -0,0 +1,52 @@
+const cheerio = require('cheerio')
+
+const createServer = require('../../../server/index')
+
+describe('Contact add route', () => {
+  const url = '/contact-add'
+  let server
+
+  beforeEach(async () => {
+    server = await createServer()
+  })
+
+  afterEach(async () => {
+    await server.stop()
+  })
+
+  test('responds with 302 when no user is logged in - GET', async () => {
+    const res = await server.inject({
+      method: 'GET',
+      url
+    })
+
+    expect(res.statusCode).toEqual(302)
+  })
+
+  test('responds with 200 when user is logged in - GET', async () => {
+    const res = await server.inject({
+      method: 'GET',
+      url,
+      auth: {
+        credentials: {
+          profile: {
+            id: 'guid',
+            email: 'test@gwa.defra.co.uk',
+            displayName: 'test gwa',
+            raw: {
+              roles: JSON.stringify([])
+            }
+          },
+          scope: []
+        },
+        strategy: 'try'
+      }
+    })
+
+    expect(res.statusCode).toEqual(200)
+    const $ = cheerio.load(res.payload)
+
+    const title = $('.govuk-label--l').text()
+    expect(title).toMatch('What is your telephone number?')
+  })
+})
