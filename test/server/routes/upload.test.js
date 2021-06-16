6ff4190b3604e79a2c146dd79c1e7b97f5c1a9d8
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-06-16 17:38:47 +0100

    feat: add test for org code validation

--- test/server/routes/upload.test.js
+++ test/server/routes/upload.test.js
@@ -261,7 +261,41 @@ describe('Upload route', () => {
         expect(res.statusCode).toEqual(500)
 
         const $ = cheerio.load(res.payload)
-        expect($('.govuk-body').text()).toEqual(`Problem uploading user data for file ${filename}`)
+        expect($('.govuk-body').text()).toEqual('Please try again later.')
+      })
+
+      test('responds with 400 and errors when org is not recognised', async () => {
+        const filename = 'test.csv'
+        const notValidOrgCode = 'not-valid-org-code'
+        const form = new FormData()
+        form.append('file', Readable.from(`${headers.join(',')}\nabc@test.com,givenName,surname,home office,07700111111`), { filename })
+        form.append('orgCode', notValidOrgCode)
+        const res = await server.inject({
+          method: 'POST',
+          url,
+          auth: {
+            credentials: {
+              user: {
+                id,
+                email,
+                displayName: 'test gwa',
+                officeCode,
+                raw: {
+                  roles: JSON.stringify([])
+                }
+              },
+              scope: [scopes.data.manage]
+            },
+            strategy: 'azuread'
+          },
+          headers: form.getHeaders(),
+          payload: await getStream(form)
+        })
+
+        expect(res.statusCode).toEqual(400)
+
+        const $ = cheerio.load(res.payload)
+        expect($('.govuk-body').text()).toEqual(`Organisation with code ${notValidOrgCode} not recognised.`)
       })
     })
   })
