9b0710826ca9c85bc6329dff071e2463f261104b
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-06-28 16:08:00 +0100

    refactor: tweak ref data heading content

--- server/lib/reference-data.js
+++ server/lib/reference-data.js
@@ -20,7 +20,7 @@ const typeInfo = {
     '<br><strong>officeCode</strong> is a generated value and will be ignored during the upload.',
     download: 'office-locations-reference-data.csv',
     filename: 'office-locations',
-    heading: 'Manage office locations reference data'
+    heading: 'office locations'
   },
   [types.orgList]: {
     blurb: 'The downloaded file will include columns: <strong>orgName</strong>, <strong>orgCode</strong>, <strong>active</strong> and <strong>core</strong>.' +
@@ -33,18 +33,19 @@ const typeInfo = {
     '</ul>',
     download: 'organisation-list-reference-data.csv',
     filename: 'org-list',
-    heading: 'Manage organisation list reference data'
+    heading: 'organisation list'
   },
   [types.orgMap]: {
     blurb: 'The downloaded file will include columns: <strong>originalOrgName</strong>, <strong>orgName</strong> and <strong>orgCode</strong>.' +
     '<br>When editing the data, either by updating existing rows or adding new rows, <strong>orgName</strong> will be ignored when the file is uploaded. The column only exists in the download to help identify which organisation the code belongs to. <strong>orgCode</strong> is used to create the mapping to the correct organisation.',
     download: 'organisation-map-reference-data.csv',
     filename: 'org-map',
-    heading: 'Manage organisation map reference data'
+    heading: 'organisation map'
   }
 }
 
 module.exports = {
+  ids,
   typeInfo,
   types
 }
