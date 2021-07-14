777b3a10c879d6f8c17f0a1199f81bd22e730de5
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-07-14 14:02:30 +0100

    feat: reduce char counts by length of service name (#21)

--- server/constants.js
+++ server/constants.js
@@ -1,3 +1,6 @@
+const notifyServiceName = 'Group Wide Alerts - Defra'
+const notifyServiceNameLength = notifyServiceName.length
+
 module.exports = {
   auditEventTypes: {
     create: 'create',
@@ -25,11 +28,14 @@ module.exports = {
     standardisedOfficeLocationMap: 'standardisedOfficeLocationMap'
   },
   textMessages: {
+    // Notify can prepend the message with the name of the service, an option
+    // that has been turned on as it helps people to identify the sms sender.
+    // Standard char allowances need to be reduced accordingly.
     oneMessageCost: 0.016,
-    oneMessageLength: 160,
-    twoMessageLength: 306,
+    oneMessageLength: 160 - notifyServiceNameLength,
+    twoMessageLength: 306 - notifyServiceNameLength,
     additionalMessageIncrement: 153,
-    maxMessageLength: 918,
+    maxMessageLength: 918 - notifyServiceNameLength,
     maxInfoLength: 2000
   }
 }
