4c365ecdc7d04cc4f6798a73eb8ca28419017161
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-07-20 15:06:50 +0100

    feat: ensure text and info are trimmed for validation

--- server/lib/route/route-validations.js
+++ server/lib/route/route-validations.js
@@ -11,8 +11,8 @@ module.exports = {
         otherwise: Joi.alternatives().try(Joi.string().pattern(/^[A-Z]{3}:/), Joi.array().min(1).items(Joi.string().pattern(/^[A-Z]{3}:/)))
       }),
       orgCodes: Joi.alternatives().try(Joi.string(), Joi.array().min(1).items(Joi.string())).required(),
-      text: Joi.string().max(maxMessageLength).required(),
-      info: Joi.string().max(maxInfoLength).allow('').empty('')
+      text: Joi.string().trim().max(maxMessageLength).required(),
+      info: Joi.string().trim().max(maxInfoLength).allow('').empty('')
     })
   }
 }
--- test/server/routes/message-create.test.js
+++ test/server/routes/message-create.test.js
@@ -191,6 +191,7 @@ describe('Message creation route', () => {
       [{ officeCodes: 'ABC:one', orgCodes: ['orgCode'], text: 'message to send', info: 'valid' }, errorMessages.allOffices],
       [{ allOffices: true, orgCodes: ['orgCode'], text: 'message to send', info: 'a'.repeat(maxInfoLength + 1) }, errorMessages.info],
       [{ allOffices: true, orgCodes: ['orgCode'], text: 'a'.repeat(maxMessageLength + 1) }, errorMessages.text],
+      [{ allOffices: true, orgCodes: ['orgCode'], text: '    ', info: 'valid' }, errorMessages.text],
       [{ allOffices: false, officeCodes: [], orgCodes: ['orgCode', 'another'], text: 'message to send', info: 'valid' }, errorMessages.officeCodes],
       [{ allOffices: false, officeCodes: [], orgCodes: 'orgCode', text: 'message to send', info: 'valid' }, errorMessages.officeCodes],
       [{ allOffices: false, officeCodes: ['ABC:one'], orgCodes: [], text: 'message to send', info: 'valid' }, errorMessages.orgCodes]
@@ -256,6 +257,7 @@ describe('Message creation route', () => {
       [{ allOffices: false, officeCodes: 'ABC:one', orgCodes: 'orgCode', text: 'message to send', info: 'valid' }],
       [{ allOffices: true, orgCodes: ['orgCode', 'another'], text: 'message to send' }],
       [{ allOffices: true, orgCodes: ['orgCode'], text: 'a'.repeat(maxMessageLength), info: 'a'.repeat(maxInfoLength) }],
+      [{ allOffices: true, orgCodes: ['orgCode'], text: 'message to send', info: ` ${'a'.repeat(maxInfoLength)} ` }],
       [{ allOffices: true, orgCodes: ['orgCode'], text: '     padded with spaces     ', info: '     padded with spaces     ' }]
     ])('responds with 302 to /messages when request is valid - test %#', async (payload) => {
       saveMessage.mockResolvedValue({ statusCode: 201 })
