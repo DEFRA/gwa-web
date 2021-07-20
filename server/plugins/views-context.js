c7b5ec6fc62549096f8e7f54ab85f8e4c290a514
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-07-20 12:44:20 +0100

    refactor: extract view checking into function

--- server/plugins/views-context.js
+++ server/plugins/views-context.js
@@ -1,6 +1,20 @@
 const { scopes } = require('../permissions')
 const { navigation: { header } } = require('../constants')
 
+function getView (request) {
+  const pathToMatch = `/${request.path.split('/')[1]}`
+  if ([header.messages.href, '/message-create', '/message-edit', '/message-delete', '/message-send', '/message-view', '/messages-sent'].includes(pathToMatch)) {
+    return header.messages.text
+  }
+
+  if ([header.account.href, '/contact-add', '/contact-edit', '/contact-remove'].includes(pathToMatch)) {
+    return header.account.text
+  }
+
+  if ([header.data.href, '/data-reference', '/data-reference-manage', '/upload'].includes(pathToMatch)) {
+    return header.data.text
+  }
+}
 /**
  * Adds an `onPreResponse` listener to apply
  * some common props to the view context.
@@ -23,27 +37,27 @@ module.exports = {
 
           const navigation = []
 
-          const pathToMatch = `/${request.path.split('/')[1]}`
+          const view = getView(request)
 
           if (auth.isAuthenticated) {
             ctx.user = auth.credentials.user
             ctx.credentials = auth.credentials
             navigation.push({
               ...header.account,
-              active: [header.account.href, '/contact-add', '/contact-edit', '/contact-remove'].includes(pathToMatch)
+              active: view === header.account.text
             })
 
             if (ctx.credentials.scope.includes(scopes.message.manage)) {
               navigation.push({
                 ...header.messages,
-                active: [header.messages.href, '/message-create', '/message-edit', '/message-delete', '/message-send', '/message-view', '/messages-sent'].includes(pathToMatch)
+                active: view === header.messages.text
               })
             }
 
             if (ctx.credentials.scope.includes(scopes.data.manage)) {
               navigation.push({
                 ...header.data,
-                active: [header.data.href, '/data-reference', '/data-reference-manage', '/upload'].includes(pathToMatch)
+                active: view === header.data.text
               })
             }
 
