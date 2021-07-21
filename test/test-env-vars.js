19066fffd303e99c1cb6a5f45a5420cf696f210f
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-07-21 18:57:31 +0100

    fix: ensure env vars are set for tests. increase consistency

--- test/test-env-vars.js
+++ test/test-env-vars.js
@@ -2,9 +2,9 @@ module.exports = {
   aadClientId: 'b8707d91-d57b-421e-8b6e-33d453a888df',
   aadClientSecret: 'b8707d91-d57b-421e-8b6e-33d453a888df',
   aadTenantId: 'b8707d91-d57b-421e-8b6e-33d453a888df',
-  contactListContainer: 'contact-list',
+  contactListContainer: 'contact-list-container',
   contactListStorageConnectionString: 'contact-list-connection-string',
-  dataSourcesContainer: 'data-sources',
+  dataSourcesContainer: 'data-sources-container',
   dataSourcesStorageConnectionString: 'data-sources-connection-string',
   cosmosDBConnectionString: 'AccountEndpoint=https://my-cosmos.documents.azure.com:443/;AccountKey=will-this-work;',
   cosmosDBName: 'gwa',
@@ -13,5 +13,7 @@ module.exports = {
   forceHttps: false,
   logoutRedirectUri: 'http://localhost.test.domain:3000',
   phaseBannerHtml: 'a message to show',
-  phaseBannerTag: 'local'
+  phaseBannerTag: 'local',
+  phoneNumbersContainer: 'phone-numbers-container',
+  phoneNumbersStorageConnectionString: 'phone-numbers-connection-string'
 }
