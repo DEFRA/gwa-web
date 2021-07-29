4015b7a7044609db633057e412246e22ab4f960b
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-07-29 17:33:16 +0100

    feat: general wording tweaks, parameterise email address

--- server/routes/faqs.js
+++ server/routes/faqs.js
@@ -1,5 +1,6 @@
 const routeId = 'faqs'
 const path = `/${routeId}`
+const { contactEmail } = require('../config')
 
 module.exports = [
   {
@@ -23,7 +24,7 @@ module.exports = [
           }, {
             heading: { text: 'Why is it important to have the Group Wide capability?' },
             content: {
-              html: '<p>Operational and administrative sites are likely to be cohabited by multiple organisations within the Group and there is likely to be travel between the respective organisations. Incidents / Events pertaining to the health and safety of any member of the workforce either operating out of a particular location site held within a specified Area , in transit to or through such a location or planning a journey need to be advised in order to take appropriate steps to maintain their wellbeing and those of their colleagues.</p>'
+              html: '<p>Operational and administrative sites are likely to be cohabited by multiple organisations within the Group and there is likely to be travel between the respective organisations. Incidents / events pertaining to the health and safety of any member of the workforce either operating out of a particular location or site held within a specified Area, in transit to or through such a location or planning a journey need to be advised in order to take appropriate steps to maintain their wellbeing and those of their colleagues.</p>'
             }
           }, {
             heading: { text: 'What sorts of messages am I likely to receive?' },
@@ -40,7 +41,7 @@ module.exports = [
           }, {
             heading: { text: 'What is the likely frequency of messages sent?' },
             content: {
-              html: '<p>Messaging is expected to be infrequent due to the consented purpose the Service is set up for, and past business continuity and security events, however due to the unpredictability of future events, the specific number of likely messages cannot be quantified, although in all cases the messaging will be contextual and limited to provide the necessary guidance when events occur.</p>'
+              html: '<p>Messaging is expected to be infrequent due to the consented purpose the Service is set up for, and past business continuity and security events, however, due to the unpredictability of future events, the specific number of likely messages cannot be quantified, although in all cases the messaging will be contextual and limited to provide the necessary guidance when events occur.</p>'
             }
           }]
         }
@@ -61,7 +62,7 @@ module.exports = [
           }, {
             heading: { text: 'Why has SMS text messaging the preferred means of communication?' },
             content: {
-              html: '<p>SMS text alerting provides the capability to issue a message of importance quickly without the reliance of the end recipient having access to Defra Group approved device or necessarily a smart phone. It therefore provides the most consistent means to disseminate information outside of the working environment or during a times when normal operation facilities are impaired in some way.</p>'
+              html: '<p>SMS text alerting provides the capability to issue a message of importance quickly without the reliance of the end recipient having access to Defra Group approved device or necessarily a smart phone. It therefore provides the most consistent means to disseminate information outside of the working environment or during times when normal operational facilities are impaired in some way.</p>'
             }
           }, {
             heading: { text: 'Who will be responsible for issuing any text messaging?' },
@@ -90,7 +91,7 @@ module.exports = [
           }, {
             heading: { text: 'Will messages always be sent to all users held within the system?' },
             content: {
-              html: '<p>Generally in the case of a major business continuity or security event it is likely that an alert will be issued to all users with a registered mobile number. However recipients can be selected based on any combination of Organisation and Area dependent on the necessity to advise specific groups.</p>'
+              html: '<p>Generally in the case of a major business continuity or security event it is likely that an alert will be issued to all users with a registered mobile number. However, recipients can be selected based on any combination of Organisation and Area dependent on the necessity to advise specific groups.</p>'
             }
           }]
         }
@@ -125,12 +126,12 @@ module.exports = [
           }, {
             heading: { text: 'Can I remove or amend my personal number entered?' },
             content: {
-              html: '<p>Yes you are back to replace or remove any numbers you have registered as well as amending or removing any Area preferences previously set up.</p>'
+              html: '<p>Yes, you are able to replace or remove the personal number you have registered as well as amending or removing any Area preferences previously set up.</p>'
             }
           }, {
             heading: { text: 'Can I remove my company mobile phone number from the system?' },
             content: {
-              html: '<p>No this is no permissible. Only additional personal numbers registered can be removed from the system.</p>'
+              html: '<p>No this is not permissible. Only additional personal numbers registered can be removed from the system.</p>'
             }
           }, {
             heading: { text: 'Can I change my base location, or organisation details if they are incorrect?' },
@@ -146,8 +147,8 @@ module.exports = [
             heading: { text: 'Can I amend my Area Location choices?' },
             content: {
               html: '<ul>' +
-                      '<li>You default official base work location is fixed within the system if you have a Company Mobile Phone and the Area it is associated with it cannot be changed, however you have the option to add new and amend additional Areas to receive specifically targeted messages.</li>' +
-                      '<li>In the case of a registered personal phone number, your preferences will be defaulted initially to your Area associated with you official base work location. However this can be removed and additional preferences added.</li>' +
+                      '<li>You default official base work location is fixed within the system if you have a Company Mobile Phone and the Area it is associated with it cannot be changed, however, you have the option to add new and amend additional Areas to receive specifically targeted messages.</li>' +
+                      '<li>In the case of a registered personal phone number, your preferences will be defaulted initially to your Area associated with your official base work location. However, this can be removed and additional preferences added.</li>' +
                     '</ul>'
             }
           }]
@@ -164,15 +165,15 @@ module.exports = [
           }, {
             heading: { text: 'How often is the Data refreshed from the Core Systems?' },
             content: {
-              html: '<p>Currently the plan is to refresh weekly on a Sunday, however the frequency of update can be amended by the System Administrators to vary this, together with an option for the administrator to invoke a further update at any time.</p>'
+              html: '<p>Currently the plan is to refresh weekly on a Sunday, however, the frequency of update can be amended by the System Administrators to vary this, together with an option for the administrator to invoke a further update at any time.</p>'
             }
           }, {
             heading: { text: 'How do I know my data is secure?' },
             content: {
               html: '<ul>' +
-                      '<li>The system data is held within Defra’s Azure Tenancy. All data held is encrypted handled by Microsoft Managed Key.</li>' +
+                      '<li>The system data is held within Defra’s Azure Tenancy. All data held is encrypted by Microsoft Managed Key.</li>' +
                       '<li>Data imported, and accessed, and issued is through HTTPS which uses TLS for encryption.</li>' +
-                      '<li>Defra security Assurance has certified the system and an independent IT Health Check was undertaken which included penetration testing to validate the security of the Service.</li>' +
+                      '<li>Defra Security Assurance has certified the system and an independent IT Health Check was undertaken which included penetration testing to validate the security of the Service.</li>' +
                     '</ul>'
             }
           }]
@@ -182,7 +183,7 @@ module.exports = [
         accordion: {
           id: 'faqs-data-management',
           items: [{
-            heading: { text: 'Why is Organisational and Area Date held within the reference module of the system?' },
+            heading: { text: 'Why is Organisational and Area data held within the reference module of the system?' },
             content: {
               html: '<p>The consolidated source data introduces some inconsistencies and omissions to which the Organisational and Area reference data resolves on importation to the system.</p>'
             }
@@ -192,7 +193,7 @@ module.exports = [
               html: '<p>System Administrators (who have full access to all functions to the system) and Data Managers who have specific privileges grant to undertake this activity.</p>'
             }
           }, {
-            heading: { text: 'How to I introduce new sites?' },
+            heading: { text: 'How do I introduce new sites?' },
             content: {
               html: '<ul>' +
                       '<li>New sites can be added by using the export function within the Data Reference module, which will generate an excel spreadsheet to append the new record(s), map to source data site information and assign to the appropriate Area. Once completed this data can be imported back into the system.</li>' +
@@ -203,7 +204,7 @@ module.exports = [
             heading: { text: 'How do I remove closed sites?' },
             content: {
               html: '<ul>' +
-                      '<li>The removal of sites follows a similar pattern as introducing a new site. Initially within the Data Reference Module use the export function to generate an excel spreadsheet. It will be necessary to ensure that any site information mapped against source data is remapped to an alternative site, in order orphaned records are not generated on import. Once all associated records from the site to be remove are remapped, the spreadsheet can be saved and imported back into the system.</li>' +
+                      '<li>The removal of sites follows a similar pattern as introducing a new site. Initially within the Data Reference Module use the export function to generate an excel spreadsheet. It will be necessary to ensure that any site information mapped against source data is remapped to an alternative site, in order orphaned records are not generated on import. Once all associated records from the site to be removed are remapped, the spreadsheet can be saved and imported back into the system.</li>' +
                       '<li>It is recommended that a system update is then executed to ensure that source data aligned with the removed and newly remapped records.</li>' +
                     '</ul>'
             }
@@ -217,16 +218,16 @@ module.exports = [
             heading: { text: 'Can messages be issued in the event of a complete system failure within Defra?' },
             content: {
               html: '<ul>' +
-                      '<li>In the unlikely event of the Defra Azure Tenancy suffering a complete failure and no alternative means to communicate to the workforce, a business continuity process is available to allow direct manual access to the Gov.</li>' +
-                      '<li>A file will be created and sent in an encrypted format to the System Administrators Group Mailbox. This is updated for every data refresh that is either undertaken on an automated scheduled or manually requested by a System Administrator.</li>' +
-                      '<li>This file can be taken offline to be uploaded to Gov.Notify with a supporting message.</li>' +
+                      '<li>In the unlikely event of the Defra Azure Tenancy suffering a complete failure and no alternative means to communicate to the workforce, a business continuity process is available to allow direct manual access to the GOV.UK Notify Service to issue messages.</li>' +
+                      '<li>A file will be created and sent in an encrypted format to the System Administrators Group Mailbox. This is updated for every data refresh that is either undertaken on an automated schedule or manually requested by a System Administrator.</li>' +
+                      '<li>This file can be taken offline to be uploaded to GOV.UK Notify with a supporting message.</li>' +
                       '<li>It should be noted that during such a business continuity event it is expected that any message prepared is issued to the complete workforce represented within the dataset.</li>' +
                     '</ul>'
             }
           }, {
             heading: { text: 'Who do I contact if I have a query?' },
             content: {
-              html: '<p>We encourage all users to access the guidance and the above FAQ’s which in most cases should answer any specific questions arising around system usage. However, should a query arise where an appropriate answer is not currently provided, please direct your question to ...@...</p>'
+              html: `<p>We encourage all users to access the guidance and the above FAQ’s which in most cases should answer any specific questions arising around system usage. However, should a query arise where an appropriate answer is not currently provided, please direct your question to <a href="mailto:${contactEmail}">${contactEmail}</a>.</p>`
             }
           }]
         }
